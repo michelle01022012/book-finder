f96986824248003ceae66851fb7538f517bef7d0
Michelle Anderson <manderson0102@yahoo.com>
2026-05-07 23:27:32 -0400

    Final fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import BookCard from '../components/BookCard';
 import SearchBar from '../components/SearchBar';
@@ -9,10 +9,18 @@ const Home = () => {
   const API_KEY = process.env.REACT_APP_GOOGLE_BOOKS_KEY;
  
 
-  const fetchBooks = async (query = 'fast') => {
-    const res = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=6&key=${API_KEY}`);
+  const fetchBooks = useCallback(async (query = 'fast') => {
+  try {
+    const res = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=6&key=${API_KEY}`
+    );
+
     setBooks(res.data.items || []);
-  };
+  } catch (error) {
+    console.error('Error fetching books:', error);
+    setBooks([]);
+  }
+}, [API_KEY]);
 
   useEffect(() => { 
      fetchBooks(); 
