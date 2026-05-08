be5f7a3fd4f28979cd60e281ba7a7e6236861dd6
Michelle Anderson <manderson0102@yahoo.com>
2026-05-07 21:05:13 -0400

    making updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,8 +31,4 @@ const Header = () => {
 
 export default Header;
 
-//const Header = () => (
-  //<header style={{ background: '#2c3e50', color: 'white', padding: '1rem', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-  //  <h1 style={{ cursor: 'pointer' }} onClick={() => window.location.href = '/'}>BookFinder</h1>
- // </header>
-//);
+
